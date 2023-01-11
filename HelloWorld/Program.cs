a1135289098624f808524f11d5beea51063c363a
Sachinmahajana <120769632+Sachinmahajana@users.noreply.github.com>
2023-01-11 08:47:51 +0530

    Making changes in Remote

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to HelloWorld Program");
+            Console.WrriteLine("Making Changes in main ");
+            Console.Readline();
 
         }
     }
